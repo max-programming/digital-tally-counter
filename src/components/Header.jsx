16ec650ad59f7940bd567d67f140c5b98e7258f4
max-programming <maxprogramming05@gmail.com>
2020-08-30 20:53:44 +0530

    Commented out Add and Menu buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ const Header = ({ setCount }) => {
             <path d="M13.5 2c-5.288 0-9.649 3.914-10.377 9h-3.123l4 5.917 4-5.917h-2.847c.711-3.972 4.174-7 8.347-7 4.687 0 8.5 3.813 8.5 8.5s-3.813 8.5-8.5 8.5c-3.015 0-5.662-1.583-7.171-3.957l-1.2 1.775c1.916 2.536 4.948 4.182 8.371 4.182 5.797 0 10.5-4.702 10.5-10.5s-4.703-10.5-10.5-10.5z" />
           </svg>
         </button>
-        <button className="add-counter">
+        {/* <button className="add-counter">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -25,10 +25,10 @@ const Header = ({ setCount }) => {
           >
             <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z" />
           </svg>
-        </button>
+        </button> */}
       </div>
       <div className="menu">
-        <button className="menu-btn">
+        {/* <button className="menu-btn">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -38,7 +38,7 @@ const Header = ({ setCount }) => {
           >
             <path d="M6 12c0 1.657-1.343 3-3 3s-3-1.343-3-3 1.343-3 3-3 3 1.343 3 3zm9 0c0 1.657-1.343 3-3 3s-3-1.343-3-3 1.343-3 3-3 3 1.343 3 3zm9 0c0 1.657-1.343 3-3 3s-3-1.343-3-3 1.343-3 3-3 3 1.343 3 3z" />
           </svg>
-        </button>
+        </button> */}
       </div>
     </div>
   );
