3e463f01ac9d1c1bf88c75f30be87783282cb0d1
max-programming <maxprogramming05@gmail.com>
2020-08-30 20:47:54 +0530

    Vibrate device and warnings

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import { Header, Counter } from "./comps";
 
 const App = () => {
   const [count, setCount] = useState(0);
-  const increment = () => setCount(count + 1);
+  const increment = () => {
+    setCount(count + 1);
+    window.navigator.vibrate(300);
+  };
   return (
     <div>
       <Header setCount={setCount} />
