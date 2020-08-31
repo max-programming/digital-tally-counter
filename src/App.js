694036c37b8392d18b22de5715e07b8e7d00c8db
max-programming <maxprogramming05@gmail.com>
2020-08-31 10:14:23 +0530

    Desktop message added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Header, Counter } from "./comps";
+import { BrowserView, MobileView } from "react-device-detect";
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -9,10 +10,27 @@ const App = () => {
   };
   return (
     <div>
-      <Header setCount={setCount} />
-      <main>
-        <Counter count={count} increment={increment} />
-      </main>
+      <BrowserView>
+        <Header setCount={setCount} />
+        <h2
+          style={{
+            color: "white",
+            textAlign: "center",
+            fontFamily: "sans-serif",
+          }}
+        >
+          Please use it in a mobile device for a better view.
+        </h2>
+        <main>
+          <Counter count={count} increment={increment} />
+        </main>
+      </BrowserView>
+      <MobileView>
+        <Header setCount={setCount} />
+        <main>
+          <Counter count={count} increment={increment} />
+        </main>
+      </MobileView>
     </div>
   );
 };
