1d6abd8f2f36bcbd2842d5cf7e234ee151d65d3e
Max Programming <ui.sabuwala@gmail.com>
2023-03-04 18:20:07 +0530

    Remove unnecessary code

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,24 +1,11 @@
-import React from "react";
+import React from 'react';
 
 const Counter = ({ count, increment }) => {
   return (
-    <div className="counter" onClick={increment}>
-      <div className="circle">
+    <div className='counter' onClick={increment}>
+      <div className='circle'>
         <h1>{count}</h1>
       </div>
-      {/* <div className="circle-wrap">
-        <div className="circle">
-          <div className="mask full">
-            <div className="fill"></div>
-          </div>
-
-          <div className="mask half">
-            <div className="fill"></div>
-          </div>
-
-          <div className="inside-circle">70%</div>
-        </div>
-      </div> */}
     </div>
   );
 };
